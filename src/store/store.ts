636f028b97fc531fe7247ed2e5fd854f55545736
niubajr19 <lucas.antonio199@outlook.com>
2022-08-21 16:09:35 -0300

    Fix - Ajustando arquivos

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import moviesReducer from "../store/ducks/moviesSlice";
+import moviesReducer from "./ducks/moviesSlice";
 
 export const store = configureStore({
   reducer: {
